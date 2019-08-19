34b18fa76037324ca33707700a9d31cd4421e0db
indu <indu@Indus-MBP13.local>
2019-08-18 22:29:12 -0400

    added search page feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,18 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	
+	public void loginPage() {
+		
+		System.out.println("Login Page");
+		
+	}
+	
+	public void loginSet() {
+		
+		System.out.println("Login set");
+		
+	}
 
 }
